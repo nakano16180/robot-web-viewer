4885c0abdd036cbdaac1951a9399318bbe815857
nakano16180 <nakano16180@gmail.com>
2020-08-06 19:45:05 +0900

    update

--- src/robot.js
+++ src/robot.js
@@ -3,18 +3,8 @@ import React, { Suspense, useRef, useState } from "react"
 import { useLoader, useFrame, extend, useThree} from "react-three-fiber"
 
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import URDFLoader from 'urdf-loader';
-
-//import customElementToReact from '@nrk/custom-element-to-react'
-//import URDFViewer from './urdf-viewer-element';
-
-//const RobotViewer = customElementToReact(URDFViewer, {
-//  props: ['package', 'urdf', 'up', 'display-shadow', 'ambient-color', 'ignore-limits']
-//});
-
 
 const LoadModel = () => {
   const robot = useLoader(URDFLoader, '/urdf/open_manipulator.URDF', loader => { 
